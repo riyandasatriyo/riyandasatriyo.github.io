e55c57c0565852576ddd3d5f2d9556a13276308a
Riyanda Satriyo <ryandasatriyo@gmail.com>
2023-01-21 10:47:38 +0700

    feat: add search feature

--- src/App.js
+++ src/App.js
@@ -3,17 +3,18 @@
 import { useEffect, useState } from 'react';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { getPokeAbility,getPokeType,getPokeMove} from './components/poke-api'
+import { getPokeSearch,getPokeAbility,getPokeType,getPokeMove} from './components/poke-api'
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
+import Card from 'react-bootstrap/Card';
 import { Stack } from 'react-bootstrap';
-import Pokelist from './components/Poke-list';
-import PokeSearch from './components/poke-search';
+import Pokelist from './components/poke-list';
 
 
 function App() {
   const [Searchs, setSearchs] = useState("");
   const [SearchValue,setSearchValue] = useState("");
+  const [PokemonSearch, setPokemonSearch] = useState([]);
   const [PokemonAbility,setPokemonAbility] = useState([]);
   const [PokemonType, setPokemonType] = useState([]);
   const [PokemonMove, setPokemonMove] = useState([]);
@@ -24,7 +25,11 @@ function App() {
      
    }
   
-
+  useEffect (() => {
+    getPokeSearch(SearchValue).then((data) => {
+      setPokemonSearch(data)
+    })
+  },[SearchValue])
 
   useEffect(() =>{
     getPokeAbility(SearchValue).then((abilities) =>{
@@ -45,6 +50,27 @@ function App() {
   },[SearchValue])
 console.log(PokemonAbility)
 
+const PokeSearch = () => {
+  return (
+    <div className="wrapper">
+      <h2>You Searched Pokemon :</h2>
+          <div className="d-flex justify-content-around">
+            <Stack direction="horizontal" gap={3}>
+              <Card style={{ width: '18rem' }}>
+                <Card.Img variant="top" src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${PokemonSearch.id}.png`} />
+                <Card.Body>
+                  <Card.Title>{PokemonSearch.name}</Card.Title>
+                  <Card.Text>
+                   
+                  </Card.Text>
+                  <Button variant="primary">View Detail</Button>
+                </Card.Body>
+              </Card>
+            </Stack>
+          </div>
+        </div>
+        )
+}
 
 
 
@@ -71,7 +97,7 @@ console.log(PokemonAbility)
       </header>
       
         <div className="Container">
-          <PokeSearch Search={SearchValue}/>
+          <PokeSearch />
         </div>
         <h2>Another Pokemon :</h2>
         <div className="Container">   
